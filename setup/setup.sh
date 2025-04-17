93fabe3e1063ac75e7f740042704fc8bd4e9b49f
xhagrg <grg.iksha@hotmail.com>
2025-04-17 15:51:56 -0500

    Update setup process.

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,12 +4,12 @@
 set -u
 
 # Replace this with the URL of your git repository
-export REPOSITORY_URL="https://github.com/nasa-impact/hls-foundation-sagemaker.git"
+export REPOSITORY_URL="https://github.com/nasa-impact/ESA-NASA-workshop-2025.git"
 export PRITHVI_EO_ENV="prithvi_eo"
 export PRITHVI_WX_ENV="prithvi_wx"
 export LLM_ENV="indus_eve"
 
-if ! [ls -la | grep -q "hls-foundation-sagemaker"]
+if ! [ls -la | grep -q "ESA-NASA-workshop-2025"]
     then
         git -C /home/sagemaker-user clone $REPOSITORY_URL
 fi
@@ -34,7 +34,7 @@ do
         conda activate $env_name
         pip install ipykernel
         python -m ipykernel install --user --name $env_name --display-name "$env_name"
-        pip install -r "hls-foundation-sagemaker/requirements.txt"
+        pip install -r "ESA-NASA-workshop-2025/$env_name/requirements.txt"
   fi
   echo "Conda env: $env_name created"
 done
