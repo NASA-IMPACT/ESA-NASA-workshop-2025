ca1e1878a601c42aac441ebca31d845146f3b8a9
xhagrg <grg.iksha@hotmail.com>
2025-04-20 23:53:14 -0500

    Update setup.sh.

--- setup/setup.sh
+++ setup/setup.sh
@@ -14,28 +14,47 @@ if ! `ls -la | grep -q "ESA-NASA-workshop-2025"`
         git -C /home/sagemaker-user clone $REPOSITORY_URL
 fi
 
-for env_name in `ls /home/sagemaker-user/ESA-NASA-workshop-2025/environments/`
-do
-  # commands to execute for each item
+function weather_setup {
+  # download weights for smaller model
+  # prepare data downloads
+  git clone https://github.com/NASA-IMPACT/Prithvi-WxC.git
+  cd Prithvi-WxC; pip install ".[example]"
+
+  # download small model weights from uah server.
+  mkdir "/home/sagemaker-user/ESA-NASA-workshop-2025/Track 1 (EO)/Prithvi-WX/notebooks/data/weights/"
+  wget -O "/home/sagemaker-user/ESA-NASA-workshop-2025/Track 1 (EO)/Prithvi-WX/notebooks/data/weights/prithvi.wxc.rollout.600m.v1.pt" https://www.nsstc.uah.edu/data/sujit.roy/demo/consolidated.pth
+}
+
+function setup() {
+  local env_name = "$1"
   if conda info --envs | grep -q $env_name
     then
         echo "$env_name already exists"
     else
-        source /opt/conda/bin/activate
-        conda create -n $env_name python=3.12 -y -q
-        # Check if the environment was created successfully
-        if [ $? -eq 0 ]; then
-          echo "Conda environment '$env_name' created successfully."
-        else
-          echo "Failed to create conda environment '$env_name'."
-          exit 1
-        fi
-        conda activate $env_name
-        pip install ipykernel
-        pip install -r "/home/sagemaker-user/ESA-NASA-workshop-2025/environments/$env_name/requirements.txt"
-        source "/home/sagemaker-user/ESA-NASA-workshop-2025/environments/$env_name/setup.sh"
-        python -m ipykernel install --user --name $env_name --display-name "$env_name"
-        conda deactivate $env_name
+      source /opt/conda/bin/activate
+      conda create -n $env_name python=3.12 -y -q
+      # Check if the environment was created successfully
+      if [ $? -eq 0 ]; then
+        echo "Conda environment '$env_name' created successfully."
+      else
+        echo "Failed to create conda environment '$env_name'."
+        exit 1
+      fi
+      conda activate $env_name
+      pip install ipykernel
+      pip install -r "/home/sagemaker-user/ESA-NASA-workshop-2025/environments/$env_name/requirements.txt"
+      if [$env_name == "prithvi_wx"]
+      then
+        weather_setup
+      python -m ipykernel install --user --name $env_name --display-name "$env_name"
+      conda deactivate
   fi
   echo "Conda env: $env_name created"
+}
+
+for env_name in `ls /home/sagemaker-user/ESA-NASA-workshop-2025/environments/`
+do
+  # commands to execute for each item
+  setup $env_name &
 done
+wait
