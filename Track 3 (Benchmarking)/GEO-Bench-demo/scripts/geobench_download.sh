3b61adb47c5f628aa62590dd56a79a6afdd18ce7
xhagrg <grg.iksha@hotmail.com>
2025-05-05 15:06:30 -0500

    Update shell scripts for data download.

--- Track 3 (Benchmarking)/GEO-Bench-demo/scripts/geobench_download.sh
+++ Track 3 (Benchmarking)/GEO-Bench-demo/scripts/geobench_download.sh
@@ -1,6 +1,8 @@
-mkdir datasets/geobench-1.0
-cd datasets/geobench-1.0
-wget -O geobench-1.0.zip https://ibm.box.com/shared/static/xuf8ysyda5t3uhqd69gq8mg5o138u0w8.zip
+#!/bin/bash
+
+mkdir "/home/sagemaker-user/ESA-NASA-workshop-2025/Track 3 (Benchmarking)/GEO-Bench-demo/datasets/geobench-1.0/"
+cd "/home/sagemaker-user/ESA-NASA-workshop-2025/Track 3 (Benchmarking)/GEO-Bench-demo/datasets/geobench-1.0/"
+wget -O geobench-1.0.zip https://ibm.box.com/shared/static/xuf8ysyda5t3uhqd69gq8mg5o138u0w8.zip --no-check-certificate
 unzip geobench-1.0.zip
 rm geobench-1.0.zip
-cd ../..
+cd ..
