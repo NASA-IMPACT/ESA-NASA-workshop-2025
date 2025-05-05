04b1bca51297d2456e80f6025476e977c4159726
Paolo Fraccaro <paolo.fraccaro@.com>
2025-05-05 21:27:34 +0000

    fixes

--- Track 3 (Benchmarking)/GEO-Bench-demo/scripts/geobench_download.sh
+++ Track 3 (Benchmarking)/GEO-Bench-demo/scripts/geobench_download.sh
@@ -1,6 +1,6 @@
 mkdir datasets/geobench-1.0
 cd datasets/geobench-1.0
-wget -O geobench-1.0.zip https://ibm.box.com/shared/static/xuf8ysyda5t3uhqd69gq8mg5o138u0w8.zip
+wget --no-check-certificate -O geobench-1.0.zip https://ibm.box.com/shared/static/xuf8ysyda5t3uhqd69gq8mg5o138u0w8.zip
 unzip geobench-1.0.zip
 rm geobench-1.0.zip
 cd ../..
--- Track 3 (Benchmarking)/GEO-Bench-demo/scripts/sen1floods11_download.sh
+++ Track 3 (Benchmarking)/GEO-Bench-demo/scripts/sen1floods11_download.sh
@@ -1,8 +1,8 @@
-cd datasets/sen1floods11
-wget -O LabelHand.zip https://ibm.box.com/shared/static/edh42df3513vtqkuxbp8g6eb2jd98xon.zip
+cd "/home/sagemaker-user/ESA-NASA-workshop-2025/Track 3 (Benchmarking)/GEO-Bench-demo/datasets/sen1flloods11/"
+wget --no-check-certificate -O LabelHand.zip https://ibm.box.com/shared/static/edh42df3513vtqkuxbp8g6eb2jd98xon.zip
 unzip LabelHand.zip
 rm LabelHand.zip
-wget -O Images.zip https://ibm.box.com/shared/static/zzg28xhjgg7thnxjsucd3efpu0b4caaa.zip
+wget --no-check-certificate -O Images.zip https://ibm.box.com/shared/static/zzg28xhjgg7thnxjsucd3efpu0b4caaa.zip
 unzip Images.zip
 rm Images.zip
 cd ..
